77fda6726116eabffd248af706d23776ff949705
HCCFish <johnson880514@gmail.com>
2023-02-22 21:18:42 +0800

    Implement q_new()
    Allocate memory for a new queue head.

--- queue.c
+++ queue.c
@@ -14,7 +14,13 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *q = malloc(sizeof(struct list_head));
+    if (!q){
+        Free(q);
+        return NULL;
+    }
+    INIT_LIST_HEAD(q);
+    return q;
 }
 
 /* Free all storage used by queue */
